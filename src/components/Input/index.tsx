cd87d9786a934d5a5b654198d66eaaa82c0c8bd0
Lucas Fontinele <contato@fontinele.dev>
2023-11-06 23:43:05 -0300

    feat: input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,22 @@
+import { ComponentProps } from "react";
+import styles from "./styles.module.css";
+
+interface InputProps extends ComponentProps<'input'> {
+  onPressEnter: () => void;
+}
+
+function Input({ onPressEnter, ...props }: InputProps) {
+  const onKeyDown = (keyCode: string | undefined) => {
+    if (!keyCode) return;
+
+    if (keyCode === 'Enter') {
+      onPressEnter();
+    }
+  }; 
+
+  return (
+    <input className={styles.inputContainer} onKeyDown={(e) => onKeyDown(e.code)} {...props} />
+  );
+}
+
+export default Input;
